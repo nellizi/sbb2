a03783fa4bc2408c156fcdb9b83d7d622415a87c
nellizi <masterliz9095@gmail.com>
2022-08-15 16:14:48 +0900

    findById를 Optional말고 오버라이딩 해서  테스트 실행

--- src/main/java/com/ll/exam/sbb2/QuestionRepository.java
+++ src/main/java/com/ll/exam/sbb2/QuestionRepository.java
@@ -2,7 +2,16 @@ package com.ll.exam.sbb2;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface QuestionRepository extends JpaRepository<Question, Integer> {
 
     Question findBySubject(String subject);
+    Question findById(int id);
+
+    Question findBySubjectAndContent(String subject, String cotent);
+
+
+    List<Question> findBySubjectLike(String subjectBites);
+    //두개 이상 나올 수 있으니까 리스트로 받기
 }
--- src/test/java/com/ll/exam/sbb2/Sbb2ApplicationTests.java
+++ src/test/java/com/ll/exam/sbb2/Sbb2ApplicationTests.java
@@ -10,48 +10,82 @@ import java.util.Optional;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @SpringBootTest
-class    Sbb2ApplicationTests {
+class Sbb2ApplicationTests {
 
-	@Autowired
-	QuestionRepository questionRepository;
-	@Autowired
-	AnswerRepository answerRepository;
-	@Test
-	void contextLoads() {
-	}
+    @Autowired
+    QuestionRepository questionRepository;
+    @Autowired
+    AnswerRepository answerRepository;
 
-	@Test
-	void jpaTest1(){
-		Question q1 = new Question();
-		q1.setSubject("what is sbb");
-		q1.setContent("wanna know about sbb");
-		q1.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q1);
+    @Test
+    void contextLoads() {
+    }
 
-		Question q2 = new Question();
-		q2.setSubject("Q about sbb model");
-		q2.setContent("does id create automatically?");
-		q2.setCreateDate(LocalDateTime.now());
-		questionRepository.save(q2);
+    @Test
+    void jpaTest1() {
+        Question q1 = new Question();
+        q1.setSubject("what is sbb");
+        q1.setContent("wanna know about sbb");
+        q1.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q1);
 
-		assertThat(q1.getId()).isGreaterThan(0);
-		assertThat(q2.getId()).isGreaterThan(q1.getId());
+        Question q2 = new Question();
+        q2.setSubject("Q about sbb model");
+        q2.setContent("does id create automatically?");
+        q2.setCreateDate(LocalDateTime.now());
+        questionRepository.save(q2);
+
+        assertThat(q1.getId()).isGreaterThan(0);
+        assertThat(q2.getId()).isGreaterThan(q1.getId());
 
 
-	}
-	@Test
-	void testJpa2(){
-		List<Question> all = questionRepository.findAll();
-		assertEquals(2,all.size());
+    }
 
-		Question question = all.get(0);
-		assertEquals("what is sbb",question.getSubject());
-	}
-		@Test
-		void testJpa3(){
-			Question q = questionRepository.findBySubject("what is sbb");
-			assertEquals(1,q.getId());
-		}
+    @Test
+    void testJpa2() {
+        List<Question> all = questionRepository.findAll();
+        assertEquals(2, all.size());
+
+        Question question = all.get(0);
+        assertEquals("what is sbb", question.getSubject());
+    }
+
+    @Test
+    void testJpa3() {
+        Question q = questionRepository.findBySubject("what is sbb");
+        assertEquals(1, q.getId());
+    }
+
+    @Test
+    void testJpa4() {
+        Question q = questionRepository.findBySubjectAndContent("what is sbb", "wanna know about sbb");
+        assertEquals(1, q.getId());
+    }
+    @Test
+    void testJpa5(){
+        List<Question>qList = questionRepository.findBySubjectLike("what%");
+        Question question = qList.get(0);
+        assertEquals("what is sbb",question.getSubject());
+    }
+
+    @Test
+    void testJpa6(){
+        Question q = questionRepository.findById(1);
+        q.setSubject("new subJect");
+        q.setContent("new Content");
+        questionRepository.save(q);
+
+//     findById는 Optional로 정의되어 있기 때문에 이렇게 사용해야 하지만
+//     QuestionReporitory에 findById 오버라이딩 해서 그냥 사용하는 방법으로 실행
+//        Optional <Question> oq = questionRepository.findById(7);
+//        assertTrue(oq.isPresent());
+//        Question question =oq.get();
+//        question.setSubject("modified title");
+//        questionRepository.save(question);
+
+    }
+
 }
